2d666c240d1f06ba03ea97cfd2970c5ff907ea8b
root <root@ROSVC-BUC000336.ce.sdxcorp.net>
2020-12-08 21:55:37 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hey Cristian!!!
+          I was change!!!
         </p>
         <a
           className="App-link"
